6c9654fd04c4b26fd97e5cbf74633247864f7383
LeNhuNgoc-Len <lenhungoc1501203@gmail.com>
2024-05-22 13:24:13 +0700

    test deplyed after fix css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col bg-[#F9F5F6]">
+    <main className="flex min-h-screen flex-col bg-[#F9F5F6]">
       <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
+      <div className="container mx-auto px-14 py-20">
         <Sections />       
       </div>
     </main>
